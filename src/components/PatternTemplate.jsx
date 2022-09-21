6d3eb312462412bd289c6c3490766a45adfd044c
Vaiva <vaistau@proton.me>
2022-09-21 15:54:39 -0500

    create component page template and example

--- src/components/PatternTemplate.jsx
+++ src/components/PatternTemplate.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import styled from "styled-components";
-import Layout from "./layout";
 
 const StyledPatternTemplate = styled.div`
-  padding: 2rem 5rem;
-  gap: 2rem;
+  padding: 2rem 1rem;
 
   h3 {
     text-transform: uppercase;
     letter-spacing: 0.3rem;
-    margin: 0;
+    margin: 0 0 3rem 4rem;
+  }
+
+  iframe {
+    width: 50rem;
+    border: 0;
+    border-radius: 10px;
+    transform: scale(1);
+    overflow: hidden;
+    margin: 1rem 0;
   }
 `;
 
-const PatternTemplate = ({ componentName, children }) => {
+const PatternTemplate = ({
+  componentName,
+  children,
+  frameTitle,
+  src,
+  height,
+}) => {
   return (
     <StyledPatternTemplate>
       <h3>{componentName}</h3>
+
       {children}
+
+      <div>
+        <iframe
+          title={frameTitle}
+          src={src}
+          style={{ height }}
+          sandbox="allow-scripts allow-same-origin"
+        />
+      </div>
     </StyledPatternTemplate>
   );
 };
