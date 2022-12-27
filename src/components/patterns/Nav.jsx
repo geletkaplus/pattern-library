07fc188ac46096d9540c1f80fb02b8fda0ae896d
Vaiva <vaistau@proton.me>
2022-12-27 14:51:06 -0600

    create nav, delete headings

--- src/components/patterns/Nav.jsx
+++ src/components/patterns/Nav.jsx
@@ -1,13 +1,44 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const StyledNav = styled.nav``;
+const StyledNav = styled.nav`
+  a {
+    font-weight: 400;
+    font-size: 1rem;
+    text-decoration: none;
+    padding: 0.5rem;
+  }
+`;
 
-const Nav = () => {
+const StyledNavLinks = styled.ul`
+  display: flex;
+  list-style-type: none;
+
+  li {
+    margin: 1.875rem 1rem;
+  }
+`;
+
+const StyledSidebar = styled.nav``;
+
+const NavLinks = () => {
   return (
+    <StyledNavLinks>
+      <li>hello</li>
+    </StyledNavLinks>
+  );
+};
+
+const Nav = sidebar => {
+  return sidebar ? (
+    <StyledSidebar>
+      <NavLinks />
+    </StyledSidebar>
+  ) : (
     <StyledNav>
-      <h3>Nav</h3>
+      <NavLinks />
     </StyledNav>
   );
 };
+
 export default Nav;
