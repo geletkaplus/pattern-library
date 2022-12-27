ea25ddad17784b9c7b004b89c736de3a1b870b32
Vaiva <vaistau@proton.me>
2022-12-27 14:26:57 -0600

    full width photo and full width video comps

--- src/components/patterns/FullWidthPhoto.jsx
+++ src/components/patterns/FullWidthPhoto.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const StyledFullWidthPhoto = styled.div``;
+const StyledFullWidthPhoto = styled.img`
+  width: 100%;
+  margin: 0 auto;
+  object-fit: fill;
 
-const FullWidthPhoto = () => {
-  return (
-    <StyledFullWidthPhoto>
-      <h3>FullWidthPhoto</h3>
-    </StyledFullWidthPhoto>
-  );
+  @media (max-width: 750px) {
+    width: 100%;
+    height: 25rem;
+    object-fit: cover;
+  }
+`;
+
+const FullWidthPhoto = ({ imageSrc, altText }) => {
+  return imageSrc && <StyledFullWidthPhoto src={imageSrc} alt={altText} />;
 };
 export default FullWidthPhoto;
