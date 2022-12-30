ee3d7eef720a8efffbe3e47f36f40785045cd483
Doug Leinen <doug@geletkaplus.com>
2022-12-30 13:19:57 -0600

    fix document in useEffect

--- src/components/GeletkaHeader.jsx
+++ src/components/GeletkaHeader.jsx
@@ -48,26 +48,27 @@ const StyledGeletkaHeader = styled.div`
 const GeletkaHeader = () => {
   const [complete, setComplete] = useState(false);
 
-  function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  async function typeSentence(sentence, eleRef, delay = 100) {
-    const letters = sentence.split('');
-    let i = 0;
-    while (i < letters.length) {
-      await waitForMs(delay);
-      if (document !== undefined) {
-        if (document.getElementById(eleRef).innerHTML !== sentence) {
-          document.getElementById(eleRef).append(letters[i]);
-          i++;
+  useEffect(() => {
+    function waitForMs(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async function typeSentence(sentence, eleRef, delay = 100) {
+      const letters = sentence.split('');
+      let i = 0;
+      while (i < letters.length) {
+        await waitForMs(delay);
+        if (typeof document !== `undefined`) {
+          if (document.getElementById(eleRef).innerHTML !== sentence) {
+            document.getElementById(eleRef).append(letters[i]);
+            i++;
+          }
         }
       }
+      return;
     }
-    return;
-  }
 
-  typeSentence('G+ Pattern Library', 'sentence');
+    typeSentence('G+ Pattern Library', 'sentence');
+  });
 
   return (
     <StyledGeletkaHeader>
